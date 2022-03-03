568827c78f751862e042831bc05709cfa84c0e3b
kimdaeseon <65288601+kimdaeseon@users.noreply.github.com>
2022-03-03 23:18:49 +0900

    Master (#2)
    * spring, jackson 의존성 추가
    
    * Application 추가
    
    * add controller hello
    
    Co-authored-by: moko.ko <moko.ko@kakaocorp.com>

--- src/main/kotlin/com/board/kopring/Application.kt
+++ src/main/kotlin/com/board/kopring/Application.kt
@@ -1,4 +1,12 @@
 package com.board.kopring
 
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
 class Application {
+}
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
 }
